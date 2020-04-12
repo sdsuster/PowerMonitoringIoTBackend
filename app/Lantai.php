c41f6aaff5fc4c3776c1c47ba112fbff0b739cb3
Jo Vianto <sjovianto@gmail.com>
2020-04-12 17:19:01 +0700

    check 3

--- app/Lantai.php
+++ app/Lantai.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lantai extends Model
 {
-    protected $table = 'Lantai';
+    protected $table = 'lantai';
     //
     protected $fillable = [
         'nama', 'id_gedung', 'created_by', 'last_updated_by',
